2c5ba8e82b8199ea38c38c167b214db4b1a63a0d
irolup <charliesavard@gmail.com>
2024-02-06 10:55:14 -0500

    Capture d'ecran fonctionnel selon un algo de couleur

--- src/application.cpp
+++ src/application.cpp
@@ -10,17 +10,18 @@ void Application::setup()
   renderer.setup();
   gui.setup("Interface");
   checkbox.setName("Gui visible");
-  checkbox = true;
+  checkbox = true; // Initialisation de l'indicateur du Gui
+  is_selecting = false; // Initialisation de l'indicateur de sélection
+  captureMode = false; // Initialisation l'indicateur du mode de capture d'écran
   gui.add(checkbox);
   group_image.setup("Image");
-  //Bouton pour echantillonnement dimage
-  screenshot_button.setup("Capture d'ecran");
+
+  // Use ofxToggle instead of ofxButton
+  screenshot_button.setup("Capture d'ecran", false);
   screenshot_button.addListener(this, &Application::screenshot_button_pressed);
+    
+
   group_image.add(&screenshot_button);
-  slider_position_x.setup("Position X", 0, 0, ofGetWidth());
-  slider_position_y.setup("Position Y", 0, 0, ofGetHeight());
-  group_image.add(&slider_position_x);
-  group_image.add(&slider_position_y);
   gui.add(&group_image);
 }
 
@@ -29,6 +30,13 @@ void Application::draw()
   renderer.draw();
   if (checkbox)
     gui.draw();
+
+  // Dessiner la sélection de zone
+  if (is_selecting)
+  {
+    ofSetColor(255, 0, 0, 100); // Couleur semi-transparente
+    ofDrawRectangle(selection_start.x, selection_start.y, selection_end.x - selection_start.x, selection_end.y - selection_start.y);
+  }
 }
 
 
@@ -51,15 +59,113 @@ void Application::keyReleased(int key)
   }
 }
 
-void Application::screenshot_button_pressed()
+void Application::screenshot_button_pressed(bool& value)
 {
-  // Faire screenshot
-  
-  ofLog() << "<Screenshot>";
+    // Activer/désactiver le mode de capture d'écran
+    captureMode = value;
+
+    if (captureMode)
+    {
+        ofLog() << "Mode de capture d'écran activé.";
+    }
+    else
+    {
+        ofLog() << "Mode de capture d'écran désactivé.";
+    }
 }
+
+void Application::mousePressed(int x, int y, int button)
+{
+  if (button == OF_MOUSE_BUTTON_LEFT)
+  {
+    // Commencer la sélection de zone
+    is_selecting = true;
+    selection_start.set(x, y);
+    selection_end.set(x, y);
+    
+  }
+}
+
+void Application::mouseDragged(int x, int y, int button)
+{
+  if (button == OF_MOUSE_BUTTON_LEFT && is_selecting)
+  {
+    // Mettre à jour la fin de la sélection de zone pendant le glissement de la souris
+    selection_end.set(x, y);
+  }
+}
+
+void Application::mouseReleased(int x, int y, int button)
+{
+    if (button == OF_MOUSE_BUTTON_LEFT && is_selecting)
+    {
+        // Finir la sélection de zone
+        selection_end.set(x, y);
+        is_selecting = false;
+
+        // Vérifier si le mode de capture d'écran est activé
+        if (captureMode)
+        {
+          // Générer un timestamp unique pour créer un nom de fichier différent à chaque capture
+            std::string timestamp = ofGetTimestampString("-%Y%m%d-%H%M%S");
+            std::string filename = "screenshot" + timestamp + ".png";
+            std::string folderPath = ofToDataPath("screenshot/");
+
+            // Construire le chemin complet du fichier dans le répertoire du binaire
+            std::string filePath = ofFilePath::join(ofFilePath::getCurrentExeDir(), folderPath + filename);
+
+            // Assurez-vous que le dossier existe
+            ofDirectory::createDirectory(folderPath, true, true);
+
+            // Effectuer la capture d'écran de la zone sélectionnée
+            int x1 = min(selection_start.x, selection_end.x);
+            int y1 = min(selection_start.y, selection_end.y);
+            int x2 = max(selection_start.x, selection_end.x);
+            int y2 = max(selection_start.y, selection_end.y);
+
+            // Créer une image temporaire pour stocker la zone sélectionnée
+            ofImage tempImage;
+            tempImage.grabScreen(x1, y1, x2 - x1, y2 - y1);
+
+            // Créer une nouvelle image pour le résultat de l'échantillonnage
+            ofImage sampledImage;
+            sampledImage.allocate(x2 - x1, y2 - y1, OF_IMAGE_COLOR);
+
+            // Algorithme d'échantillonnage
+            for (int i = 0; i < sampledImage.getWidth(); ++i)
+            {
+                for (int j = 0; j < sampledImage.getHeight(); ++j)
+                {
+                    // Algorithme d'échantillonnage ici
+                    // Exemple : simplement copier la couleur
+                    ofColor pixelColor = tempImage.getColor(i, j);
+                    sampledImage.setColor(i, j, pixelColor);
+                }
+            }
+
+            // Mettre à jour les pixels de l'image résultante
+            sampledImage.update();
+
+            // Enregistrer l'image résultante avec le nom et l'emplacement fournis par l'utilisateur
+            sampledImage.save(filePath);
+
+            ofLog() << "<Screenshot de la zone sélectionnée avec échantillonnage>";
+
+        }
+        else
+        {
+            ofLog() << "<Fin de la sélection de zone>";
+        }
+    }
+}
+
+
+
+
 
 void Application::exit()
 {
-  screenshot_button.removeListener(this, &Application::screenshot_button_pressed);
-  ofLog() << "<app::exit>";
+  // Remove the listener for toggle button valueChanged event
+    screenshot_button.removeListener(this, &Application::screenshot_button_pressed);
+    ofLog() << "<app::exit>";
 }
--- src/application.h
+++ src/application.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "ofMain.h"
 #include "ofxGui.h"
+#include "ofSystemUtils.h"
 #include "renderer.h"
 class Application : public ofBaseApp
 {
@@ -12,16 +13,29 @@ public:
 
   ofxGuiGroup group_image;
 
-  ofxFloatSlider slider_position_x;
-  ofxFloatSlider slider_position_y;
   ofParameter<bool> checkbox;
+  ofEvent<bool> screenshotButtonEvent;  // Declare an ofEvent
+  
+  //Screenshot selection points
+  ofPoint selection_start;
+  ofPoint selection_end;
 
-  ofxButton screenshot_button;
+  ofxToggle screenshot_button;
   void setup();
   void update();
   void draw();
   void exit();
   void keyReleased(int key);
-  void screenshot_button_pressed();
+  void screenshot_button_pressed(bool& value);
+
+
+  // Nouvelles fonctions pour la gestion de la sélection de zone (utile pour screenshot)
+  void mousePressed(int x, int y, int button) override;
+  void mouseDragged(int x, int y, int button) override;
+  void mouseReleased(int x, int y, int button) override;
+
+  private:
+    bool is_selecting;  // Indicateur de sélection en cours
+    bool captureMode; //Indicateur de mode CaptureDecran
 
 };
